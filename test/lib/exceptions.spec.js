f57e9f8d5e87b57f3587e1ffacc070a0ec7d63a4
CPatchane <code@patchane.com>
2017-09-07 16:33:11 +0200

    test: add test for exceptions library :white_check_mark:

---
+++ test/lib/exceptions.spec.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/* eslint-env jest */
+
+import * as exceptions from '../../src/lib/exceptions'
+
+const exceptionProperties = [
+  'ForbiddenException',
+  'ServerErrorException',
+  'NotFoundException',
+  'MethodNotAllowedException',
+  'UnavailableStackException',
+  'UnavailableSettingsException',
+  'UnauthorizedStackException',
+  'NotUninstallableAppException'
+]
+
+const statusCodes = [
+  '401', '403', '404', '405', '500'
+]
+
+describe('Exceptions library', () => {
+  it('should have correct exeception classes', () => {
+    exceptionProperties.map(p => {
+      expect(exceptions).toHaveProperty(p)
+      const testInstance = new exceptions[p]()
+      expect(testInstance).toHaveProperty('name')
+      expect(testInstance).toHaveProperty('message')
+      expect(testInstance).toHaveProperty('stack')
+    })
+  })
+  it('should have correct error statuses properties', () => {
+    expect(exceptions).toHaveProperty('errorStatuses')
+    statusCodes.map(s => {
+      expect(exceptions.errorStatuses).toHaveProperty(s)
+    })
+  })
+})
