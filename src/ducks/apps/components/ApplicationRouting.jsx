a6906599c14102084d5cc3e5aa5eb0966ec7bc9f
CPatchane <code@patchane.com>
2017-10-06 00:54:06 +0200

    fix: ApplicationDetails and ApplicationRouting improvements :pencil2:

--- src/ducks/apps/components/ApplicationRouting.jsx
+++ src/ducks/apps/components/ApplicationRouting.jsx
@@ -1,31 +1,27 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
-
-import { translate } from 'cozy-ui/react/I18n'
+import { Route, withRouter } from 'react-router-dom'
 
 import ApplicationDetails from './ApplicationDetails'
 import UninstallModal from './UninstallModal'
 
 export class ApplicationRouting extends Component {
-  openApp (related) {
-    window.location.href = related
-  }
-
   render () {
-    const { t, installedApps, isFetching, parent } = this.props
+    const { installedApps, isFetching, parent, history } = this.props
     return (
       <div>
         <Route path={`/${parent}/:appSlug`} render={({ match }) => {
           if (isFetching) return
           if (installedApps.length && match.params) {
             const app = installedApps.find(app => app.slug === match.params.appSlug)
-            return <ApplicationDetails t={t} app={app} openApp={this.openApp} parent={parent} />
+            if (!app) return history.push(`/${parent}`)
+            return <ApplicationDetails app={app} parent={parent} />
           }
         }} />
         <Route path={`/${parent}/:appSlug/manage`} render={({ match }) => {
           if (isFetching) return
           if (installedApps.length && match.params) {
             const app = installedApps.find(app => app.slug === match.params.appSlug)
+            if (!app) return history.push(`/${parent}`)
             return <UninstallModal uninstallApp={this.props.uninstallApp} parent={parent} error={this.props.actionError} app={app} match={match} />
           }
         }} />
@@ -34,4 +30,4 @@ export class ApplicationRouting extends Component {
   }
 }
 
-export default translate()(ApplicationRouting)
+export default withRouter(ApplicationRouting)
