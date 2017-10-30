a38768430810b9a1270e5a80fb946b573b41d821
CPatchane <code@patchane.com>
2017-10-30 14:43:23 +0100

    feat: add emoji parsing as option in ReactMarkdownWrapper :sparkles:

--- src/ducks/components/ReactMarkdownWrapper.jsx
+++ src/ducks/components/ReactMarkdownWrapper.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import Emoji from 'emoji-js'
+
+const emojiParser = new Emoji()
 
 export const reactMarkdownRendererOptions = {
   Link: props => <a href={props.href} target='_blank'>{props.children}</a>
 }
 
-export const ReactMarkdownWrapper = ({ source }) =>
+export const ReactMarkdownWrapper = ({ source, parseEmoji }) =>
   <ReactMarkdown
-    source={source}
+    source={parseEmoji ? emojiParser.replace_colons(source) : source}
     renderers={reactMarkdownRendererOptions}
   />
 
