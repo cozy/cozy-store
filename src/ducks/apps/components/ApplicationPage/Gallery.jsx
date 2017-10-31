afaea5e1da194bc4d0e6d111a7458c440d7e896c
CPatchane <code@patchane.com>
2017-11-01 00:54:57 +0100

    feat: better screenshots gallery on application page :nail_care:

--- src/ducks/apps/components/ApplicationPage/Gallery.jsx
+++ src/ducks/apps/components/ApplicationPage/Gallery.jsx
@@ -3,12 +3,34 @@ import React from 'react'
 import { translate } from 'cozy-ui/react/I18n'
 
 export const Gallery = ({t, slug, images}) => {
+  if (!images) return null
+  const imagesParts = images.reduce((acc, image, index) => {
+    if (index < 2) {
+      if (!acc.big) acc.big = []
+      acc.big.push(image)
+    } else if (index < 5) {
+      if (!acc.small) acc.small = []
+      acc.small.push(image)
+    }
+    return acc
+  }, {})
   return (
     <div className='sto-app-images'>
-      {images && !!images.length &&
-        images.map((screen, index) =>
-          <img className='sto-app-image' src={screen} alt={`${slug}-image-${index + 1}`} />
-        )
+      {imagesParts.big && !!imagesParts.big.length &&
+        <div className='sto-app-big-images'>
+          {imagesParts.big.map((image, index) =>
+            <img className='sto-app-image' src={image} alt={`${slug}-image-${index + 1}`} />
+          )}
+        </div>
+      }
+      {imagesParts.small && !!imagesParts.small.length &&
+        <div
+          className={`sto-app-small-images${imagesParts.small.length > 2 ? ' --space-around' : ''}`}
+        >
+          {imagesParts.small.map((image, index) =>
+            <img className='sto-app-small-image' src={image} alt={`${slug}-image-${index + 3}`} />
+          )}
+        </div>
       }
     </div>
   )
