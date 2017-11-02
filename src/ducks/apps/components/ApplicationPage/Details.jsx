cda746a90d46a7242bc3be45746794bad355d8be
CPatchane <code@patchane.com>
2017-11-02 14:00:46 +0100

    feat: better handling mobile app informations on the app page :sparkles:

--- src/ducks/apps/components/ApplicationPage/Details.jsx
+++ src/ducks/apps/components/ApplicationPage/Details.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 import { translate } from 'cozy-ui/react/I18n'
+import Icon from 'cozy-ui/react/Icon'
 import ReactMarkdownWrapper from 'ducks/components/ReactMarkdownWrapper'
+
+const isValidUrl = (url) => {
+  if (!url) return null
+  url.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/)
+}
 
 export const Details = ({t, description, changes, category, langs, mobileApps, developer}) => {
   const langsInfos = langs.map(l => t(`app_langs.${l}`))
@@ -42,13 +48,24 @@ export const Details = ({t, description, changes, category, langs, mobileApps, d
             {langsInfos.join(t('app_langs.list_separator'))}
           </div>
         </div>
-        {mobileApps &&
+        {mobileApps && !!mobileApps.length &&
           <div className='sto-app-info'>
             <div className='sto-app-info-header'>
               {t('app_page.infos.mobile_app')}
             </div>
-            <div className='sto-app-info-content'>
-              {t('app_page.infos.yes')}
+            <div className='sto-app-info-content sto-app-info-content--mobile-apps'>
+              {mobileApps.map(a => {
+                const icon = require(`assets/icons/platforms/icon-${a.type}.svg`)
+                return (
+                  <a
+                    className='sto-app-info-content-icon'
+                    href={isValidUrl(a.url) ? a.url : null}
+                    target='_blank'
+                  >
+                    <Icon icon={icon.default} width='24px' height='24px' />
+                  </a>
+                )
+              })}
             </div>
           </div>
         }
@@ -56,7 +73,7 @@ export const Details = ({t, description, changes, category, langs, mobileApps, d
         {developer &&
           <div className='sto-app-developer-infos'>
             {developerName && <span>{developerName}</span>}
-            {developer.url && <a href={developer.url} className='sto-app-developer-link' target='_blank'>
+            {isValidUrl(developer.url) && <a href={developer.url} className='sto-app-developer-link' target='_blank'>
               {developer.url}
             </a>}
           </div>
--- src/ducks/apps/components/ApplicationPage/index.jsx
+++ src/ducks/apps/components/ApplicationPage/index.jsx
@@ -7,12 +7,24 @@ import Gallery from './Gallery'
 import Details from './Details'
 import { getLocalizedAppProperty } from 'ducks/apps'
 
+const MOBILE_PLATFORMS = ['ios', 'android']
+const isMobilePlatform = (name) => MOBILE_PLATFORMS.includes(name)
+
 export const ApplicationPage = ({t, lang, app, parent}) => {
   const { icon, installed, editor, related, slug } = app
   const appName = getLocalizedAppProperty(app, 'name', lang)
   const appShortDesc = getLocalizedAppProperty(app, 'short_description', lang)
   const appLongDesc = getLocalizedAppProperty(app, 'long_description', lang)
   const appChanges = getLocalizedAppProperty(app, 'changes', lang)
+  const mobileApps = app.platforms && !!app.platforms.length &&
+    app.platforms.reduce((mobilePlatforms, platformInfos) => {
+      // force to be lower case
+      platformInfos.type = platformInfos.type.toLowerCase()
+      if (isMobilePlatform(platformInfos.type)) {
+        mobilePlatforms.push(platformInfos)
+      }
+      return mobilePlatforms
+    }, [])
   return (
     <div className='sto-app'>
       <Header
@@ -30,6 +42,7 @@ export const ApplicationPage = ({t, lang, app, parent}) => {
         category={app.category}
         langs={app.langs}
         developer={app.developer}
+        mobileApps={mobileApps}
       />
     </div>
   )
