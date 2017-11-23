b805101eaeac43580bbde513c20e674ccf769cab
CPatchane <code@patchane.com>
2017-11-23 10:23:18 +0100

    fix: get back ApplicationPage as a stateless functional component :pencil2:

--- src/ducks/apps/components/ApplicationPage/index.jsx
+++ src/ducks/apps/components/ApplicationPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { translate } from 'cozy-ui/react/I18n'
 import Spinner from 'cozy-ui/react/Spinner'
@@ -11,67 +11,64 @@ import { getLocalizedAppProperty } from 'ducks/apps'
 const MOBILE_PLATFORMS = ['ios', 'android']
 const isMobilePlatform = (name) => MOBILE_PLATFORMS.includes(name)
 
-export class ApplicationPage extends Component {
-  render () {
-    const { t, lang, parent, app, isFetching, fetchError } = this.props
-    if (isFetching) {
-      return (
-        <div className='sto-app'>
-          <Spinner
-            size='xxlarge'
-            loadingType='appsFetching'
-            middle='true'
-          />
-        </div>
-      )
-    }
-    if (fetchError) {
-      return (
-        <p class='coz-error'>
-          {t('app_modal.install.message.version_error', {message: fetchError.message})}
-        </p>
-      )
-    }
-    const { icon, installed, editor, related, slug } = app
-    const appName = getLocalizedAppProperty(app, 'name', lang)
-    const appShortDesc = getLocalizedAppProperty(app, 'short_description', lang)
-    const appLongDesc = getLocalizedAppProperty(app, 'long_description', lang)
-    const appChanges = getLocalizedAppProperty(app, 'changes', lang)
-    const mobileApps = app.platforms && !!app.platforms.length &&
-      app.platforms.reduce((mobilePlatforms, platformInfos) => {
-        // force to be lower case
-        platformInfos.type = platformInfos.type.toLowerCase()
-        if (isMobilePlatform(platformInfos.type)) {
-          mobilePlatforms.push(platformInfos)
-        }
-        return mobilePlatforms
-      }, [])
+export const ApplicationPage = ({ t, lang, parent, app, isFetching, fetchError }) => {
+  if (isFetching) {
     return (
-      <div>
-        <div className='sto-app'>
-          <Header
-            icon={icon}
-            editor={editor}
-            name={appName}
-            description={appShortDesc}
-            installed={installed}
-            installedAppLink={related}
-            parent={parent}
-            slug={slug}
-          />
-          <Gallery slug={slug} images={app.screenshots} />
-          <Details
-            description={appLongDesc}
-            changes={appChanges}
-            category={app.category}
-            langs={app.langs}
-            developer={app.developer}
-            mobileApps={mobileApps}
-          />
-        </div>
+      <div className='sto-app'>
+        <Spinner
+          size='xxlarge'
+          loadingType='appsFetching'
+          middle='true'
+        />
       </div>
     )
   }
+  if (fetchError) {
+    return (
+      <p class='coz-error'>
+        {t('app_modal.install.message.version_error', {message: fetchError.message})}
+      </p>
+    )
+  }
+  const { icon, installed, editor, related, slug } = app
+  const appName = getLocalizedAppProperty(app, 'name', lang)
+  const appShortDesc = getLocalizedAppProperty(app, 'short_description', lang)
+  const appLongDesc = getLocalizedAppProperty(app, 'long_description', lang)
+  const appChanges = getLocalizedAppProperty(app, 'changes', lang)
+  const mobileApps = app.platforms && !!app.platforms.length &&
+    app.platforms.reduce((mobilePlatforms, platformInfos) => {
+      // force to be lower case
+      platformInfos.type = platformInfos.type.toLowerCase()
+      if (isMobilePlatform(platformInfos.type)) {
+        mobilePlatforms.push(platformInfos)
+      }
+      return mobilePlatforms
+    }, [])
+  return (
+    <div>
+      <div className='sto-app'>
+        <Header
+          icon={icon}
+          editor={editor}
+          name={appName}
+          description={appShortDesc}
+          installed={installed}
+          installedAppLink={related}
+          parent={parent}
+          slug={slug}
+        />
+        <Gallery slug={slug} images={app.screenshots} />
+        <Details
+          description={appLongDesc}
+          changes={appChanges}
+          category={app.category}
+          langs={app.langs}
+          developer={app.developer}
+          mobileApps={mobileApps}
+        />
+      </div>
+    </div>
+  )
 }
 
 // translate is needed here for the lang props
