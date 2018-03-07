3db08ff6250612381f35e732d7b84b51aa0dfd08
CPatchane <code@patchane.com>
2018-03-07 17:20:35 +0100

    feat: add install/open on small app items :sparkles:

--- test/components/smallAppItem.spec.js
+++ test/components/smallAppItem.spec.js
@@ -6,7 +6,10 @@ import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import SmallAppItem from '../../src/ducks/components/SmallAppItem'
+import { tMock } from '../jestLib/I18n'
+import { Button } from 'cozy-ui/react/Button'
+import { Link } from 'react-router-dom'
+import { SmallAppItem } from '../../src/ducks/components/SmallAppItem'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -32,6 +35,7 @@ const appMock2 = {
   icon: '<svg></svg>',
   name: 'Test2',
   version: '3.0.3-beta7483',
+  installedAppLink: 'test2.cozy.mock',
   installed: true,
   onClick: jest.fn()
 }
@@ -52,24 +56,24 @@ const appMockWithoutIcon = {
 
 describe('SmallAppItem component', () => {
   it('should be rendered correctly an app', () => {
-    const component = shallow(<SmallAppItem {...appMock} />).getElement()
+    const component = shallow(<SmallAppItem t={tMock} {...appMock} />).getElement()
     expect(component).toMatchSnapshot()
   })
 
   it('should be rendered correctly an installed app', () => {
-    const component = shallow(<SmallAppItem {...appMock2} />).getElement()
+    const component = shallow(<SmallAppItem t={tMock} {...appMock2} />).getElement()
     expect(component).toMatchSnapshot()
   })
 
   it('should be rendered correctly an installed app without icon provided', () => {
     const component = shallow(
-      <SmallAppItem {...appMockWithoutIcon} />
+      <SmallAppItem t={tMock} {...appMockWithoutIcon} />
     ).getElement()
     expect(component).toMatchSnapshot()
   })
 
   it('should not call onClick when Enter is not pressed', () => {
-    const component = shallow(<SmallAppItem {...appMock} />)
+    const component = shallow(<SmallAppItem t={tMock} {...appMock} />)
     expect(component.find('div.sto-small-app-item').length).toBe(1)
     component
       .find('div.sto-small-app-item')
@@ -78,11 +82,32 @@ describe('SmallAppItem component', () => {
   })
 
   it('should call onClick when Enter is pressed', () => {
-    const component = shallow(<SmallAppItem {...appMock} />)
+    const component = shallow(<SmallAppItem t={tMock} {...appMock} />)
     expect(component.find('div.sto-small-app-item').length).toBe(1)
     component
       .find('div.sto-small-app-item')
       .simulate('keydown', { keyCode: 13 })
     expect(appMock.onClick.mock.calls.length).toBe(1)
+  })
+
+  it('should redirect correctly to the app if installed', () => {
+    window.location.assign = jest.fn()
+    const mockEventStopPropagation = jest.fn()
+    const component = shallow(<SmallAppItem t={tMock} {...appMock2} />)
+    component
+      .find(Button)
+      .simulate('click', { stopPropagation: mockEventStopPropagation })
+    expect(window.location.assign.mock.calls.length).toBe(1)
+    expect(window.location.assign.mock.calls[0][0]).toBe(appMock2.installedAppLink)
+    expect(mockEventStopPropagation.mock.calls.length).toBe(1)
+  })
+
+  it('should open the app installing modal if not installed', () => {
+    const mockEventStopPropagation = jest.fn()
+    const component = shallow(<SmallAppItem t={tMock} {...appMock} />)
+    const linkComponent = component.find(Link)
+    expect(linkComponent.getElement().props.to).toBe(`/discover/${appMock.slug}/manage`)
+    linkComponent.simulate('click', { stopPropagation: mockEventStopPropagation })
+    expect(mockEventStopPropagation.mock.calls.length).toBe(1)
   })
 })
