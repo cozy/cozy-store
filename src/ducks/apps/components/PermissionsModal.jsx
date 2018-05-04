2e31f4ac3510a8f0bef6a1ca6169858d72dfa848
CPatchane <code@patchane.com>
2018-05-04 14:12:23 +0200

    :nail_care: fix: add title to permissions modal

--- src/ducks/apps/components/PermissionsModal.jsx
+++ src/ducks/apps/components/PermissionsModal.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
-import Modal, { ModalContent } from 'cozy-ui/react/Modal'
+import Modal, { ModalContent, ModalHeader } from 'cozy-ui/react/Modal'
+import { translate } from 'cozy-ui/react/I18n'
+
 import { withRouter } from 'react-router-dom'
 import PermissionsList from './PermissionsList'
 
@@ -15,9 +17,12 @@ export class PermissionsModal extends Component {
   }
 
   render(props) {
-    const { app } = props
+    const { t, app } = props
     return (
       <Modal secondaryAction={() => this.gotoParent()} mobileFullscreen>
+        <ModalHeader className="sto-install-header">
+          <h2>{t('app_modal.permissions.title')}</h2>
+        </ModalHeader>
         <ModalContent>
           <PermissionsList app={app} />
         </ModalContent>
@@ -26,4 +31,4 @@ export class PermissionsModal extends Component {
   }
 }
 
-export default withRouter(PermissionsModal)
+export default withRouter(translate()(PermissionsModal))
