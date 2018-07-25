4eef05650d05a2f94216ac2c9a08c779a0ebfc2d
CPatchane <code@patchane.com>
2018-07-25 09:28:52 +0200

    :sparkles: feat: use brand new install modal with transparency label

---
+++ src/ducks/apps/components/Install/InstallModalFooter.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+import { ModalFooter } from 'cozy-ui/react/Modal'
+
+import { translate } from 'cozy-ui/react/I18n'
+import Icon from 'cozy-ui/react/Icon'
+
+import cozySmileIcon from 'assets/icons/icon-cozy-smile.svg'
+
+class InstallModalFooter extends Component {
+  installApp() {
+    this.setState({ error: null })
+    const { app, onError, channel } = this.props
+    this.props.installApp(app.slug, app.type, channel).catch(error => {
+      if (onError) return onError(error)
+      throw error
+    })
+  }
+
+  render() {
+    const {
+      fetchError,
+      installError,
+      isFetching,
+      isInstalling,
+      isCanceling,
+      t
+    } = this.props
+
+    const isFirstLoading = isFetching && !isCanceling
+    return (
+      !isFirstLoading &&
+      !fetchError && (
+        <ModalFooter>
+          {installError && (
+            <p className="u-error">
+              {t('app_modal.install.message.install_error', {
+                message: installError.message
+              })}
+            </p>
+          )}
+          <div className="sto-install-controls">
+            <button
+              role="button"
+              disabled={isInstalling || isCanceling}
+              aria-busy={isInstalling}
+              className="c-btn c-btn--regular c-btn--download"
+              onClick={() => this.installApp()}
+            >
+              <Icon
+                icon={cozySmileIcon}
+                color="#FFFFFF"
+                width="16px"
+                height="16px"
+                className="sto-app-install-button-icon"
+              />
+              <span>{t('app_modal.install.install')}</span>
+            </button>
+          </div>
+        </ModalFooter>
+      )
+    )
+  }
+}
+
+export default translate()(InstallModalFooter)
