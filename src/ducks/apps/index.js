d5e470591e3980f507dc2be71b09438ee61e399e
CPatchane <code@patchane.com>
2018-07-27 12:27:57 +0200

    :bug: fix: undefined label issue

--- src/ducks/apps/index.js
+++ src/ducks/apps/index.js
@@ -512,7 +512,7 @@ export async function getFormattedRegistryApp(
     Number.isInteger(responseApp.label) &&
     responseApp.label <= 5 &&
     responseApp.label >= 0
-      ? LABELS[label]
+      ? LABELS[responseApp.label]
       : null
   return Object.assign(
     {},
