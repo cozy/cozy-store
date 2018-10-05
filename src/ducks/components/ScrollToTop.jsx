650ccf5d9ee8c24e29175c3af27d7826b1703ea0
CPatchane <code@patchane.com>
2018-10-05 12:42:39 +0200

    fix: :bug: Fix scrollToTop on mobile

--- src/ducks/components/ScrollToTop.jsx
+++ src/ducks/components/ScrollToTop.jsx
@@ -1,13 +1,19 @@
 import { Component } from 'react'
 import { withRouter } from 'react-router'
+import withBreakpoints from 'cozy-ui/react/helpers/withBreakpoints'
 
 // a component to automatically reset the scroll
 // on mount (see https://reacttraining.com/react-router/web/guides/scroll-restoration/scroll-to-top)
 export class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
     if (this.props.location !== prevProps.location) {
-      const domNode = this.props.target && this.props.target.getDOMNode()
-      domNode.scrollTo(0, 0)
+      if (!this.props.breakpoints.isDesktop) {
+        // on mobile we scroll on <html />
+        document.documentElement.scrollTo(0, 0)
+      } else {
+        const domNode = this.props.target && this.props.target.getDOMNode()
+        domNode.scrollTo(0, 0)
+      }
     }
   }
 
@@ -16,4 +22,4 @@ export class ScrollToTop extends Component {
   }
 }
 
-export default withRouter(ScrollToTop)
+export default withRouter(withBreakpoints()(ScrollToTop))
