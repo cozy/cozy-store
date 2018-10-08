54c1ddf67a8daefbd08a4013e47bcc774562d025
Grégory Le Garec <gregory.legarec@gmail.com>
2018-10-08 16:36:51 +0200

    refacto: App status helpers 🛠

--- src/ducks/apps/appStatus.js
+++ src/ducks/apps/appStatus.js
@@ -1,23 +1,21 @@
 export const getCurrentStatusLabel = app => {
-  const { installed, maintenance, availableVersion } = app
-  if (availableVersion) return 'update'
+  const { installed, maintenance } = app
+  if (hasPendingUpdate(app)) return 'update'
   if (maintenance) return 'maintenance'
   if (installed) return 'installed'
   return null
 }
 
 export const hasPendingUpdate = app => {
-  if (app.availableVersion) return true
-  return false
+  const { availableVersion, version } = app
+  return availableVersion && availableVersion !== version
 }
 
 export const isUnderMaintenance = app => {
-  if (!app.availableVersion && app.maintenance) return true
-  return false
+  return !hasPendingUpdate(app) && app.maintenance
 }
 
 /* installed here means no actions needed */
 export const isInstalledAndNothingToReport = app => {
-  if (!app.availableVersion && !app.maintenance && app.installed) return true
-  return false
+  return !hasPendingUpdate(app) && !isUnderMaintenance(app) && app.installed
 }
