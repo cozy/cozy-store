7cd7e557af370663b85bed8378a919c4cb809e62
Patrick Browne <pt.browne@gmail.com>
2020-09-20 17:23:20 +0200

    chore: Lint

--- src/ducks/apps/components/Sections/components/AppsSection.jsx
+++ src/ducks/apps/components/Sections/components/AppsSection.jsx
@@ -5,28 +5,21 @@ import { getTranslatedManifestProperty } from '../helpers'
 import sortBy from 'lodash/sortBy'
 import StoreAppItem from './StoreAppItem'
 
-
 const makeNameGetter = t => app => getTranslatedManifestProperty(app, 'name', t)
 
-export const AppsSection = ({
-  t,
-  appsList,
-  subtitle,
-  onAppClick,
-  breakpoints = {}
-}) => {
+export const AppsSection = ({ t, appsList, subtitle, onAppClick }) => {
   return (
     <div className="sto-sections-apps">
       {subtitle}
       {appsList && !!appsList.length && (
         <div className="sto-sections-list">
-          {sortBy(appsList, makeNameGetter(t)).map(app =>
+          {sortBy(appsList, makeNameGetter(t)).map(app => (
             <StoreAppItem
               app={app}
               key={app.slug}
               onClick={() => onAppClick(app.slug)}
             />
-          )}
+          ))}
         </div>
       )}
     </div>
