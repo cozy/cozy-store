9070344c2759152fff1f649a59d94ea82982f41c
Quentin Valmori <quentin.valmori@gmail.com>
2021-05-11 18:57:12 +0200

    Revert "refactor: Use Dialog for PermissionsModal"
    This reverts commit 6c4cffc9f92dc47cf92f703505ca94909202583b.

--- src/ducks/apps/components/PermissionsModal.jsx
+++ src/ducks/apps/components/PermissionsModal.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
 
+import Modal, {
+  ModalContent,
+  ModalHeader
+} from 'cozy-ui/transpiled/react/Modal'
 import { translate } from 'cozy-ui/transpiled/react/I18n'
-import { Dialog } from 'cozy-ui/transpiled/react/CozyDialogs'
+import Portal from 'cozy-ui/transpiled/react/Portal'
 
+import { withRouter } from 'react-router-dom'
 import PermissionsList from 'ducks/apps/components/PermissionsList'
 
 export class PermissionsModal extends Component {
@@ -18,14 +22,17 @@ export class PermissionsModal extends Component {
 
   render() {
     const { t, app } = this.props
-
     return (
-      <Dialog
-        open={true}
-        onClose={() => this.gotoParent()}
-        title={t('app_modal.permissions.title')}
-        content={<PermissionsList app={app} />}
-      />
+      <Portal into="body">
+        <Modal secondaryAction={() => this.gotoParent()} mobileFullscreen>
+          <ModalHeader className="sto-install-header">
+            <h2>{t('app_modal.permissions.title')}</h2>
+          </ModalHeader>
+          <ModalContent>
+            <PermissionsList app={app} />
+          </ModalContent>
+        </Modal>
+      </Portal>
     )
   }
 }
