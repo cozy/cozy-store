facd46d30e05177fe5958893384d7de49b806e9a
Yannick Chiron <yannick.chiron@gmail.com>
2022-09-30 15:50:36 +0200

    fragment

--- src/ducks/components/AppRouter.jsx
+++ src/ducks/components/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
 import { Discover, MyApplications } from 'ducks/apps/Containers'
