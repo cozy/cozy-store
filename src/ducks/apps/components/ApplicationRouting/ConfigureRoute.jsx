51bddfb726e065df05781a3b152f70362f11f4d3
Ldoppea <yannick.chiron@gmail.com>
2022-11-04 19:08:46 +0100

    feat: Make ApplicationRouting compatible with react-router v6
    React-router v6 handles nested route differently from v4
    
    Now `Route` component should be placed directly inside a `Routes`
    component
    
    Also it seems not to be possible to have multiple `Routes` side by
    side. When doing so, cozy-store was going into an infinite loop
    
    So the route déclaration has been moved to the parent component where
    all cozy-app routes are declared
    
    More info: https://reactrouter.com/en/main/start/faq#how-do-i-nest-routes-deep-in-the-tree

--- src/ducks/apps/components/ApplicationRouting/ConfigureRoute.jsx
+++ src/ducks/apps/components/ApplicationRouting/ConfigureRoute.jsx
@@ -1,29 +1,22 @@
 import React from 'react'
-import { Route, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import ConfigureModal from 'ducks/apps/components/ConfigureModal'
 
 export const ConfigureRoute = ({ getApp, isFetching, parent, redirectTo }) => {
   const params = useParams()
+  if (isFetching) return null
+  const app = getApp(params)
+  if (!app) return redirectTo(`/${parent}`)
+  const appPath = `/${parent}/${app.slug}`
+  const redirectToApp = () => redirectTo(appPath)
   return (
-    <Route
-      path={`/${parent}/:appSlug/configure`}
-      render={() => {
-        if (isFetching) return null
-        const app = getApp(params)
-        if (!app) return redirectTo(`/${parent}`)
-        const appPath = `/${parent}/${app.slug}`
-        const redirectToApp = () => redirectTo(appPath)
-        return (
-          <ConfigureModal
-            appSlug={app.slug}
-            dismissAction={redirectToApp}
-            onNotInstalled={redirectToApp}
-            onSuccess={redirectToApp}
-            onWebApp={redirectToApp}
-          />
-        )
-      }}
+    <ConfigureModal
+      appSlug={app.slug}
+      dismissAction={redirectToApp}
+      onNotInstalled={redirectToApp}
+      onSuccess={redirectToApp}
+      onWebApp={redirectToApp}
     />
   )
 }
--- src/ducks/apps/components/ApplicationRouting/UninstallRoute.jsx
+++ src/ducks/apps/components/ApplicationRouting/UninstallRoute.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
-import { Route, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import UninstallModal from 'ducks/apps/components/UninstallModal'
 
 export const UninstallRoute = ({ getApp, isFetching, parent, redirectTo }) => {
   const params = useParams()
 
+  if (isFetching) return null
+
+  const parentPath = `/${parent}`
+  const app = getApp(params)
+  if (!app) return redirectTo(parentPath)
+
+  const appPath = `${parentPath}/${app.slug}`
   return (
-    <Route
-      path={`/${parent}/:appSlug/uninstall`}
-      render={() => {
-        if (isFetching) return null
-
-        const parentPath = `/${parent}`
-        const app = getApp(params)
-        if (!app) return redirectTo(parentPath)
-
-        const appPath = `${parentPath}/${app.slug}`
-        return (
-          <UninstallModal
-            appSlug={app.slug}
-            dismissAction={() => redirectTo(appPath)}
-            onNotInstalled={() => redirectTo(appPath)}
-            onSuccess={() => redirectTo(appPath)}
-          />
-        )
-      }}
+    <UninstallModal
+      appSlug={app.slug}
+      dismissAction={() => redirectTo(appPath)}
+      onNotInstalled={() => redirectTo(appPath)}
+      onSuccess={() => redirectTo(appPath)}
     />
   )
 }
