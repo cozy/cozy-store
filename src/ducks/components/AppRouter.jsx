589590b7a9f20df87d32b3148edc60f22f915e64
cballevre <celestin.ballevre@protonmail.com>
2022-12-19 14:00:54 +0100

    fix(AppRouter.jsx): make discover the default route

--- src/ducks/components/AppRouter.jsx
+++ src/ducks/components/AppRouter.jsx
@@ -10,7 +10,7 @@ export const AppRouter = () => {
       <Route path="redirect" render={() => <IntentRedirect />} />
       <Route path={`discover/*`} element={<Discover />} />
       <Route path={`myapps/*`} element={<MyApplications />} />
-      <Route path="*" element={<Navigate replace to="myapps" />} />
+      <Route path="*" element={<Navigate replace to="discover" />} />
     </Routes>
   )
 }
