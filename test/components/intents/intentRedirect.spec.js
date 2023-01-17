1bcdc596351ebf14cb7d726ce81b6d8df93759b2
cballevre <celestin.ballevre@protonmail.com>
2023-01-17 10:14:47 +0100

    fix: Test on IntentRedirect

--- test/components/intents/intentRedirect.spec.js
+++ test/components/intents/intentRedirect.spec.js
@@ -4,37 +4,31 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
+import { useLocation } from 'react-router-dom'
 
 import { IntentRedirect } from 'ducks/components/intents/IntentRedirect'
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: jest.fn()
+}))
+
 describe('IntentRedirect component', () => {
   it('should be rendered correctly if url search provided', () => {
-    const location = {
-      search: '?doctype=io.cozy.apps'
-    }
-    const component = shallow(
-      <IntentRedirect location={location} />
-    ).getElement()
+    useLocation.mockReturnValue({ search: '?doctype=io.cozy.apps' })
+    const component = shallow(<IntentRedirect />).getElement()
     expect(component).toMatchSnapshot()
   })
 
   it('should be rendered correctly if search for slug', () => {
-    const location = {
-      search: '?slug=mock'
-    }
-    const component = shallow(
-      <IntentRedirect location={location} />
-    ).getElement()
+    useLocation.mockReturnValue({ search: '?slug=mock' })
+    const component = shallow(<IntentRedirect />).getElement()
     expect(component).toMatchSnapshot()
   })
 
   it('should handle search param without value', () => {
-    const location = {
-      search: '?slug=mock&doctype'
-    }
-    const component = shallow(
-      <IntentRedirect location={location} />
-    ).getElement()
+    useLocation.mockReturnValue({ search: '?slug=mock&doctype' })
+    const component = shallow(<IntentRedirect />).getElement()
     expect(component).toMatchSnapshot()
   })
 })
