16f33534bde4d95dd2e3da546904e92f0117b35b
Merkur39 <merkkur39@gmail.com>
2023-01-20 17:59:29 +0100

    refactor(AppVote): Use i18n hook instead HOC

--- src/ducks/components/AppVote.jsx
+++ src/ducks/components/AppVote.jsx
@@ -1,32 +1,38 @@
 import React from 'react'
-import voteIllustration from 'assets/icons/app-vote.svg'
+
 import Icon from 'cozy-ui/transpiled/react/Icon'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import PopupOpener from 'cozy-ui/transpiled/react/PopupOpener'
+
+import voteIllustration from 'assets/icons/app-vote.svg'
 
 const VOTING_LINK = 'https://feedback.cozy.io/'
 
-export const AppVote = ({ t }) => (
-  <PopupOpener
-    url={VOTING_LINK}
-    title="Cozy app voting"
-    height={700}
-    width={650}
-    className="sto-app-vote-wrapper"
-  >
-    <div className="sto-app-vote">
-      <Icon
-        icon={voteIllustration}
-        height="60"
-        width="55"
-        className="sto-app-vote-icon"
-      />
-      <strong>
-        <p>{t('app_vote.line1')}</p>
-        <p>{t('app_vote.line2')}</p>
-      </strong>
-    </div>
-  </PopupOpener>
-)
+export const AppVote = () => {
+  const { t } = useI18n()
 
-export default translate()(AppVote)
+  return (
+    <PopupOpener
+      url={VOTING_LINK}
+      title="Cozy app voting"
+      height={700}
+      width={650}
+      className="sto-app-vote-wrapper"
+    >
+      <div className="sto-app-vote">
+        <Icon
+          icon={voteIllustration}
+          height="60"
+          width="55"
+          className="sto-app-vote-icon"
+        />
+        <strong>
+          <p>{t('app_vote.line1')}</p>
+          <p>{t('app_vote.line2')}</p>
+        </strong>
+      </div>
+    </PopupOpener>
+  )
+}
+
+export default AppVote
