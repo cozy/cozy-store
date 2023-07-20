ac15fe4d8f08ea5d43ad2cadb050668306535b53
Ldoppea <yannick.chiron@gmail.com>
2023-07-20 11:57:36 +0200

    refactor: Convert `<PermissionsModal />` to functional component

--- src/ducks/apps/components/PermissionsModal.jsx
+++ src/ducks/apps/components/PermissionsModal.jsx
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import Modal, {
   ModalContent,
   ModalHeader
 } from 'cozy-ui/transpiled/react/Modal'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Portal from 'cozy-ui/transpiled/react/Portal'
 
 import PermissionsList from 'ducks/apps/components/PermissionsList'
 
-export class PermissionsModal extends Component {
-  gotoParent() {
-    const { app, parent, navigate } = this.props
+export const PermissionsModal = ({ app, parent }) => {
+  const { t } = useI18n()
+  const navigate = useNavigate()
+
+  const gotoParent = () => {
     if (app && app.slug) {
       navigate(`${parent}/${app.slug}`)
     } else {
@@ -20,26 +22,18 @@ export class PermissionsModal extends Component {
     }
   }
 
-  render() {
-    const { t, app } = this.props
-    return (
-      <Portal into="body">
-        <Modal secondaryAction={() => this.gotoParent()} mobileFullscreen>
-          <ModalHeader className="sto-install-header">
-            <h2>{t('app_modal.permissions.title')}</h2>
-          </ModalHeader>
-          <ModalContent>
-            <PermissionsList app={app} />
-          </ModalContent>
-        </Modal>
-      </Portal>
-    )
-  }
+  return (
+    <Portal into="body">
+      <Modal secondaryAction={() => gotoParent()} mobileFullscreen>
+        <ModalHeader className="sto-install-header">
+          <h2>{t('app_modal.permissions.title')}</h2>
+        </ModalHeader>
+        <ModalContent>
+          <PermissionsList app={app} />
+        </ModalContent>
+      </Modal>
+    </Portal>
+  )
 }
 
-const PermissionsModalWrapper = props => {
-  const navigate = useNavigate()
-  return <PermissionsModal {...props} navigate={navigate} />
-}
-
-export default translate()(PermissionsModalWrapper)
+export default PermissionsModal
