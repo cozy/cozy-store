60970e231853d40199f8b7a681702159c4f1aa22
Ldoppea <yannick.chiron@gmail.com>
2023-07-20 11:50:38 +0200

    refactor: Convert `<Details />` to functional component

--- src/ducks/apps/components/ApplicationPage/Details.jsx
+++ src/ducks/apps/components/ApplicationPage/Details.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { withClient } from 'cozy-client'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useClient } from 'cozy-client'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { Button } from 'cozy-ui/transpiled/react/Button'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import Toggle from 'cozy-ui/transpiled/react/Toggle'
@@ -30,203 +30,192 @@ const isValidUrl = url => {
   )
 }
 
-export class Details extends Component {
-  constructor(props) {
-    super(props)
-    const { app } = this.props
-    const appChannel = getChannel(app.source)
-    this.state = {
-      displayBetaChannel: appChannel === REGISTRY_CHANNELS.BETA,
-      displayDevChannel: appChannel === REGISTRY_CHANNELS.DEV
-    }
+export const Details = ({ app, description, changes, parent, mobileApps }) => {
+  const { source, slug, langs, categories, developer, version } = app
 
-    this.toggleChannels = this.toggleChannels.bind(this)
-    this.onUpdateChannel = this.onUpdateChannel.bind(this)
+  const appChannel = getChannel(source)
+
+  const client = useClient()
+  const { t } = useI18n()
+  const navigate = useNavigate()
+
+  const [displayBetaChannel, setDisplayBetaChannel] = useState(
+    appChannel === REGISTRY_CHANNELS.BETA
+  )
+  const [displayDevChannel, setDisplayDevChannel] = useState(
+    appChannel === REGISTRY_CHANNELS.DEV
+  )
+
+  const isBeta = appChannel === REGISTRY_CHANNELS.BETA
+  const isDev = appChannel === REGISTRY_CHANNELS.DEV
+  const langsInfos = langs && langs.map(l => t(`app_langs.${l}`))
+  const categoriesInfos =
+    categories &&
+    !!categories.length &&
+    categories.map(c => t(`app_categories.${c}`))
+  const developerName =
+    developer && getTranslatedManifestProperty(app, 'developer.name', t)
+  const developerUrl =
+    developer && getTranslatedManifestProperty(app, 'developer.url', t)
+  const shortVersion = version && version.match(/^(\d+\.\d+\.\d+)-.*$/)
+  const displayedVersion =
+    (shortVersion && shortVersion.length && shortVersion[1]) || version
+
+  const onShowPermissions = () => {
+    navigate(`/${parent}/${app.slug}/permissions`)
   }
 
-  onShowPermissions() {
-    const { app, parent } = this.props
-    this.props.navigate(`/${parent}/${app.slug}/permissions`)
-  }
-
-  async toggleChannels() {
-    const { app } = this.props
+  const toggleChannels = async () => {
     // only for installed apps and apps from the registry
     if (!(app.installed && getChannel(app.source))) return
-    const stateUpdate = {}
-    if (!this.state.displayBetaChannel) {
-      stateUpdate.displayBetaChannel = true
-    }
-    if (!this.state.displayDevChannel) {
-      const context = await getContext(this.props.client)
-      if (context && context.attributes && context.attributes.debug) {
-        stateUpdate.displayDevChannel = true
+
+    const context = await getContext(client)
+
+    setDisplayBetaChannel(display => !display)
+    setDisplayDevChannel(display => {
+      if (
+        !display &&
+        context &&
+        context.attributes &&
+        context.attributes.debug
+      ) {
+        return true
       }
-    }
-    this.setState(() => stateUpdate)
+      return false
+    })
   }
 
-  onUpdateChannel(checked, channel) {
-    const { app, navigate, parent } = this.props
+  const onUpdateChannel = (checked, channel) => {
     const targetChannel = checked ? channel : REGISTRY_CHANNELS.STABLE
     navigate(`/${parent}/${app.slug}/channel/${targetChannel}`)
   }
 
-  render() {
-    const { t, app, description, changes, mobileApps } = this.props
-    const { displayBetaChannel, displayDevChannel } = this.state
-    const { source, slug, langs, categories, developer, version } = app
-    const appChannel = getChannel(source)
-    const isBeta = appChannel === REGISTRY_CHANNELS.BETA
-    const isDev = appChannel === REGISTRY_CHANNELS.DEV
-    const langsInfos = langs && langs.map(l => t(`app_langs.${l}`))
-    const categoriesInfos =
-      categories &&
-      !!categories.length &&
-      categories.map(c => t(`app_categories.${c}`))
-    const developerName =
-      developer && getTranslatedManifestProperty(app, 'developer.name', t)
-    const developerUrl =
-      developer && getTranslatedManifestProperty(app, 'developer.url', t)
-    const shortVersion = version && version.match(/^(\d+\.\d+\.\d+)-.*$/)
-    const displayedVersion =
-      (shortVersion && shortVersion.length && shortVersion[1]) || version
-    return (
-      <div className="sto-app-details">
-        <div className="sto-app-descriptions">
-          {isUnderMaintenance(app) && <Maintenance slug={slug} />}
-          {description && (
-            <div className="sto-app-description">
-              <h3 className="u-title-h3">{t('app_page.description')}</h3>
-              <ReactMarkdownWrapper source={description} parseEmoji />
-            </div>
-          )}
-          {changes && (
-            <div className="sto-app-changes">
-              <h3 className="u-title-h3">{t('app_page.changes')}</h3>
-              <ReactMarkdownWrapper source={changes} parseEmoji />
-            </div>
-          )}
+  return (
+    <div className="sto-app-details">
+      <div className="sto-app-descriptions">
+        {isUnderMaintenance(app) && <Maintenance slug={slug} />}
+        {description && (
+          <div className="sto-app-description">
+            <h3 className="u-title-h3">{t('app_page.description')}</h3>
+            <ReactMarkdownWrapper source={description} parseEmoji />
+          </div>
+        )}
+        {changes && (
+          <div className="sto-app-changes">
+            <h3 className="u-title-h3">{t('app_page.changes')}</h3>
+            <ReactMarkdownWrapper source={changes} parseEmoji />
+          </div>
+        )}
+      </div>
+      <div className="sto-app-additional-details">
+        <h3 className="u-title-h3">{t('app_page.infos.title')}</h3>
+        <div className="sto-app-info">
+          <div className="sto-app-info-header">
+            {t('app_page.infos.categories')}
+          </div>
+          <div className="sto-app-info-content">
+            {categoriesInfos &&
+              categoriesInfos.join(t('app_categories.list_separator'))}
+          </div>
         </div>
-        <div className="sto-app-additional-details">
-          <h3 className="u-title-h3">{t('app_page.infos.title')}</h3>
+        <div className="sto-app-info">
+          <div className="sto-app-info-header">
+            {t('app_page.infos.version.title')}
+          </div>
+          <div className="sto-app-info-content">
+            <span onDoubleClick={toggleChannels}>
+              {displayedVersion || t('app_page.infos.version.unknown')}
+            </span>
+          </div>
+        </div>
+        {(displayBetaChannel || displayDevChannel) && (
           <div className="sto-app-info">
             <div className="sto-app-info-header">
-              {t('app_page.infos.categories')}
+              {t('app_page.infos.beta')}
             </div>
             <div className="sto-app-info-content">
-              {categoriesInfos &&
-                categoriesInfos.join(t('app_categories.list_separator'))}
+              <Toggle
+                id={`sto-app-${slug}-beta-toggle`}
+                checked={isBeta}
+                onToggle={e => onUpdateChannel(e, REGISTRY_CHANNELS.BETA)}
+              />
             </div>
           </div>
+        )}
+        {displayDevChannel && (
+          <div className="sto-app-info">
+            <div className="sto-app-info-header">{t('app_page.infos.dev')}</div>
+            <div className="sto-app-info-content">
+              <Toggle
+                id={`sto-app-${slug}-dev-toggle`}
+                checked={isDev}
+                onToggle={e => onUpdateChannel(e, REGISTRY_CHANNELS.DEV)}
+              />
+            </div>
+          </div>
+        )}
+        {langsInfos && (
           <div className="sto-app-info">
             <div className="sto-app-info-header">
-              {t('app_page.infos.version.title')}
+              {t('app_page.infos.langs')}
             </div>
             <div className="sto-app-info-content">
-              <span onDoubleClick={this.toggleChannels}>
-                {displayedVersion || t('app_page.infos.version.unknown')}
-              </span>
+              {langsInfos.join(t('app_langs.list_separator'))}
             </div>
           </div>
-          {(displayBetaChannel || displayDevChannel) && (
-            <div className="sto-app-info">
-              <div className="sto-app-info-header">
-                {t('app_page.infos.beta')}
-              </div>
-              <div className="sto-app-info-content">
-                <Toggle
-                  id={`sto-app-${slug}-beta-toggle`}
-                  checked={isBeta}
-                  onToggle={e =>
-                    this.onUpdateChannel(e, REGISTRY_CHANNELS.BETA)
-                  }
-                />
-              </div>
+        )}
+        {mobileApps && !!mobileApps.length && (
+          <div className="sto-app-info">
+            <div className="sto-app-info-header">
+              {t('app_page.infos.mobile_app')}
             </div>
-          )}
-          {displayDevChannel && (
-            <div className="sto-app-info">
-              <div className="sto-app-info-header">
-                {t('app_page.infos.dev')}
-              </div>
-              <div className="sto-app-info-content">
-                <Toggle
-                  id={`sto-app-${slug}-dev-toggle`}
-                  checked={isDev}
-                  onToggle={e => this.onUpdateChannel(e, REGISTRY_CHANNELS.DEV)}
-                />
-              </div>
-            </div>
-          )}
-          {langsInfos && (
-            <div className="sto-app-info">
-              <div className="sto-app-info-header">
-                {t('app_page.infos.langs')}
-              </div>
-              <div className="sto-app-info-content">
-                {langsInfos.join(t('app_langs.list_separator'))}
-              </div>
-            </div>
-          )}
-          {mobileApps && !!mobileApps.length && (
-            <div className="sto-app-info">
-              <div className="sto-app-info-header">
-                {t('app_page.infos.mobile_app')}
-              </div>
-              <div className="sto-app-info-content sto-app-info-content--mobile-apps">
-                {mobileApps.map(a => {
-                  const icon = platformIcons[a.type]
-                  return (
-                    <a
-                      className="sto-app-info-content-icon"
-                      href={isValidUrl(a.url) ? a.url : null}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      key={a.type}
-                    >
-                      <Icon icon={icon} width="24px" height="24px" />
-                    </a>
-                  )
-                })}
-              </div>
-            </div>
-          )}
-          <div>
-            <Button
-              label={t('app_page.permissions.button.label')}
-              className="sto-app-permissions-button"
-              onClick={() => this.onShowPermissions()}
-              subtle
-            />
-          </div>
-          {developerName && (
-            <div>
-              <h3 className="u-title-h3">{t('app_page.developer_infos')}</h3>
-              <div className="sto-app-developer-infos">
-                <span>{developerName}</span>
-                {isValidUrl(developerUrl) && (
+            <div className="sto-app-info-content sto-app-info-content--mobile-apps">
+              {mobileApps.map(a => {
+                const icon = platformIcons[a.type]
+                return (
                   <a
-                    href={developerUrl}
-                    className="sto-app-developer-link"
+                    className="sto-app-info-content-icon"
+                    href={isValidUrl(a.url) ? a.url : null}
                     target="_blank"
                     rel="noopener noreferrer"
+                    key={a.type}
                   >
-                    {developerUrl}
+                    <Icon icon={icon} width="24px" height="24px" />
                   </a>
-                )}
-              </div>
+                )
+              })}
             </div>
-          )}
+          </div>
+        )}
+        <div>
+          <Button
+            label={t('app_page.permissions.button.label')}
+            className="sto-app-permissions-button"
+            onClick={() => onShowPermissions()}
+            subtle
+          />
         </div>
+        {developerName && (
+          <div>
+            <h3 className="u-title-h3">{t('app_page.developer_infos')}</h3>
+            <div className="sto-app-developer-infos">
+              <span>{developerName}</span>
+              {isValidUrl(developerUrl) && (
+                <a
+                  href={developerUrl}
+                  className="sto-app-developer-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {developerUrl}
+                </a>
+              )}
+            </div>
+          </div>
+        )}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-const DetailsWrapper = props => {
-  const navigate = useNavigate()
-  return <Details {...props} navigate={navigate} />
-}
-
-export default withClient(translate()(DetailsWrapper))
+export default Details
