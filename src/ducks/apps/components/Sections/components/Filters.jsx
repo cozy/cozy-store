92099c101f03a3a08e7cc4963ab0a5054ce44254
cballevre <celestin.ballevre@protonmail.com>
2023-08-31 09:03:31 +0200

    refactor(Filters): Migrate to ActionsMenu

--- src/ducks/apps/components/Sections/components/Filters.jsx
+++ src/ducks/apps/components/Sections/components/Filters.jsx
@@ -1,19 +1,21 @@
 /* global cozy */
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, forwardRef } from 'react'
 
+import ActionsMenu from 'cozy-ui/transpiled/react/ActionsMenu'
+import { makeActions } from 'cozy-ui/transpiled/react/ActionsMenu/Actions'
+import ActionsMenuItem from 'cozy-ui/transpiled/react/ActionsMenu/ActionsMenuItem'
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import IconButton from 'cozy-ui/transpiled/react/IconButton'
 import SettingIcon from 'cozy-ui/transpiled/react/Icons/Setting'
-import ActionMenu, {
-  ActionMenuItem,
-  ActionMenuRadio
-} from 'cozy-ui/transpiled/react/deprecated/ActionMenu'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import Radio from 'cozy-ui/transpiled/react/Radios'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
 const Filters = ({ filter, onFilterChange }) => {
-  const anchorRef = useRef()
+  const anchorRef = useRef(null)
   const [menuDisplayed, setMenuDisplayed] = useState(false)
 
   const areAppsUnderMaintenanceShown = filter.showMaintenance
@@ -26,25 +28,60 @@ const Filters = ({ filter, onFilterChange }) => {
     setMenuDisplayed(false)
   }
 
-  const showAppUnderMaintenance = () => {
-    if (!areAppsUnderMaintenanceShown) {
-      onFilterChange({
-        ...filter,
-        showMaintenance: true
-      })
-    }
-  }
-
-  const hideAppUnderMaintenance = () => {
-    if (areAppsUnderMaintenanceShown) {
-      delete filter.showMaintenance
-      onFilterChange(filter)
-    }
-  }
-
   const { isMobile } = useBreakpoints()
   const { t } = useI18n()
   const { BarRight } = cozy.bar
+
+  const showAppUnderMaintenance = () => ({
+    name: 'showAppUnderMaintenance',
+    action: () => {
+      if (!areAppsUnderMaintenanceShown) {
+        onFilterChange({
+          ...filter,
+          showMaintenance: true
+        })
+      }
+    },
+    Component: forwardRef(function ShowAppUnderMaintenanceItem(props, ref) {
+      return (
+        <ActionsMenuItem {...props} ref={ref}>
+          <ListItemIcon>
+            <Radio checked={areAppsUnderMaintenanceShown} />
+          </ListItemIcon>
+          <ListItemText
+            primary={t('sections.filters.under_maintenance.show')}
+          />
+        </ActionsMenuItem>
+      )
+    })
+  })
+
+  const hideAppUnderMaintenance = () => ({
+    name: 'hideAppUnderMaintenance',
+    action: () => {
+      if (areAppsUnderMaintenanceShown) {
+        delete filter.showMaintenance
+        onFilterChange(filter)
+      }
+    },
+    Component: forwardRef(function HideAppUnderMaintenanceItem(props, ref) {
+      return (
+        <ActionsMenuItem {...props} ref={ref}>
+          <ListItemIcon>
+            <Radio checked={!areAppsUnderMaintenanceShown} />
+          </ListItemIcon>
+          <ListItemText
+            primary={t('sections.filters.under_maintenance.hide')}
+          />
+        </ActionsMenuItem>
+      )
+    })
+  })
+
+  const actions = makeActions([
+    showAppUnderMaintenance,
+    hideAppUnderMaintenance
+  ])
 
   return (
     <>
@@ -54,6 +91,8 @@ const Filters = ({ filter, onFilterChange }) => {
             onClick={toggleMenu}
             ref={anchorRef}
             aria-label={t('sections.filters.action')}
+            aria-controls="simple-menu"
+            aria-haspopup="true"
           >
             <Icon icon={SettingIcon} />
           </IconButton>
@@ -65,31 +104,22 @@ const Filters = ({ filter, onFilterChange }) => {
           onClick={toggleMenu}
           ref={anchorRef}
           startIcon={<Icon icon={SettingIcon} />}
+          aria-controls="simple-menu"
+          aria-haspopup="true"
         />
       )}
 
       {menuDisplayed && (
-        <ActionMenu
-          anchorElRef={anchorRef}
-          autoclose={true}
-          onClose={hideMenu}
-          popperOptions={{
-            placement: 'bottom-end'
+        <ActionsMenu
+          open
+          ref={anchorRef}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right'
           }}
-        >
-          <ActionMenuItem
-            onClick={showAppUnderMaintenance}
-            left={<ActionMenuRadio checked={areAppsUnderMaintenanceShown} />}
-          >
-            {t('sections.filters.under_maintenance.show')}
-          </ActionMenuItem>
-          <ActionMenuItem
-            onClick={hideAppUnderMaintenance}
-            left={<ActionMenuRadio checked={!areAppsUnderMaintenanceShown} />}
-          >
-            {t('sections.filters.under_maintenance.hide')}
-          </ActionMenuItem>
-        </ActionMenu>
+          actions={actions}
+          onClose={hideMenu}
+        />
       )}
     </>
   )
