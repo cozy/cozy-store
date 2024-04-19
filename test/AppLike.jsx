e0c422d8a22e084c9668850c3d963aaccccdb518
cballevre <celestin.ballevre@protonmail.com>
2024-04-19 16:53:59 +0200

    test: Make them work after updating cozy-ui & cozy-bar

--- test/AppLike.jsx
+++ test/AppLike.jsx
@@ -1,4 +1,3 @@
-import store from 'lib/store'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
@@ -9,16 +8,22 @@ import I18n from 'cozy-ui/transpiled/react/providers/I18n'
 
 import enLocale from '../src/locales/en.json'
 
-const AppLike = ({ children, store, client }) => (
-  <Provider store={store || (client && client.store)}>
-    <CozyProvider client={client || createMockClient({})}>
-      <I18n dictRequire={() => enLocale} lang="en">
-        <BreakpointsProvider>
-          <HashRouter>{children}</HashRouter>
-        </BreakpointsProvider>
-      </I18n>
-    </CozyProvider>
-  </Provider>
-)
-
+const AppLike = ({ children, store, client }) => {
+  const mockClient = createMockClient({})
+  return (
+    <Provider
+      store={
+        store || (client && client.store) || (mockClient && mockClient.store)
+      }
+    >
+      <CozyProvider client={client || mockClient}>
+        <I18n dictRequire={() => enLocale} lang="en">
+          <BreakpointsProvider>
+            <HashRouter>{children}</HashRouter>
+          </BreakpointsProvider>
+        </I18n>
+      </CozyProvider>
+    </Provider>
+  )
+}
 export default AppLike
--- test/jestLib/setup.js
+++ test/jestLib/setup.js
@@ -6,25 +6,7 @@ import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
-const React = require('react')
-
 // polyfill for requestAnimationFrame
 global.requestAnimationFrame = cb => {
   setTimeout(cb, 0)
 }
-
-global.cozy = {
-  bar: {
-    // eslint-disable-next-line react/display-name
-    BarCenter: ({ children }) => <div>{children}</div>
-  }
-}
-
-// To avoid the errors while creating theme (since no CSS stylesheet
-// defining CSS variables is injected during tests)
-// Material-UI: the color provided to augmentColor(color) is invalid.
-// The color object needs to have a `main` property or a `500` property.
-jest.mock('cozy-ui/transpiled/react/utils/color', () => ({
-  getCssVariableValue: () => '#fff',
-  getInvertedCssVariableValue: () => '#000'
-}))
