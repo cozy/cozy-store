67e195bda8ea28f95afb49693cfae7b8582a2975
Antonin Cezard <acezard@gmail.com>
2024-07-03 15:41:04 +0200

    feat: add needed queries for shortcuts retrieval

--- src/ducks/queries.ts
+++ src/ducks/queries.ts
@@ -6,15 +6,15 @@ interface QueryConfig {
   options: QueryOptions
 }
 
-type QueryBuilder = () => QueryConfig
+type QueryBuilder<T = void> = (params: T) => QueryConfig
 
-const older30s = 30 * 1000
+const fetchPolicy = CozyClient.fetchPolicies.olderThan(30 * 1000) // 30s
 
 export const buildOauthClientsQuery: QueryBuilder = () => ({
   definition: Q('io.cozy.oauth.clients'),
   options: {
     as: 'io.cozy.oauth.clients',
-    fetchPolicy: CozyClient.fetchPolicies.olderThan(older30s)
+    fetchPolicy
   }
 })
 
@@ -24,6 +24,75 @@ export const buildDisplaySettingsQuery: QueryBuilder = () => ({
     .indexFields(['_id']),
   options: {
     as: 'io.cozy.settings.display',
-    fetchPolicy: CozyClient.fetchPolicies.olderThan(older30s)
+    fetchPolicy
+  }
+})
+
+interface MkHomeCustomShortcutsDirConnParams {
+  currentFolderId?: string
+  type?: string
+  sortAttribute?: string
+  sortOrder?: string
+}
+
+export const mkHomeCustomShorcutsDirConn: QueryBuilder<
+  MkHomeCustomShortcutsDirConnParams
+> = ({
+  currentFolderId = '',
+  type = 'directory',
+  sortAttribute = 'name',
+  sortOrder = 'asc'
+}) => ({
+  definition: Q('io.cozy.files')
+    .where({
+      dir_id: currentFolderId,
+      type,
+      [sortAttribute]: { $gt: null }
+    })
+    .partialIndex({
+      _id: {
+        $ne: 'io.cozy.files.trash-dir'
+      }
+    })
+    .indexFields(['dir_id', 'type', sortAttribute])
+    .sortBy([
+      { dir_id: sortOrder },
+      { type: sortOrder },
+      { [sortAttribute]: sortOrder }
+    ])
+    .limitBy(100),
+  options: {
+    as: `${type} ${currentFolderId} ${sortAttribute} ${sortOrder}`,
+    fetchPolicy
+  }
+})
+
+export const mkHomeCustomShorcutsConn: QueryBuilder<string[]> = foldersId => ({
+  definition: Q('io.cozy.files')
+    .where({
+      class: 'shortcut',
+      dir_id: {
+        $in: foldersId
+      },
+      name: { $gt: null }
+    })
+    .indexFields(['class', 'dir_id', 'name'])
+    .sortBy([{ class: 'asc' }, { dir_id: 'asc' }, { name: 'asc' }])
+    .limitBy(100),
+  options: {
+    as: 'home-shortcuts',
+    fetchPolicy
+  }
+})
+
+export const mkHomeMagicFolderConn: QueryBuilder<
+  (key: string) => string
+> = t => ({
+  definition: Q('io.cozy.files')
+    .where({ path: t('home_config_magic_folder') })
+    .indexFields(['path']),
+  options: {
+    as: 'home/io.cozy.files/path=magic-folder',
+    fetchPolicy
   }
 })
