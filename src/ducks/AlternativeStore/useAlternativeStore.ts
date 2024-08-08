78f8e210bebfe255276ee1858b3daf7cd4a1b0ad
Antonin Cezard <acezard@gmail.com>
2024-08-08 15:11:12 +0200

    refactor: remove all toutatice mentions

--- src/ducks/AlternativeStore/useAlternativeStore.ts
+++ src/ducks/AlternativeStore/useAlternativeStore.ts
@@ -1,6 +1,8 @@
 import { useMemo } from 'react'
+
 import { useQuery } from 'cozy-client'
 import flag from 'cozy-flags'
+import { useExtendI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import { buildShortcutsQuery } from 'ducks/queries'
 import { transformData } from 'ducks/AlternativeStore/transformData'
@@ -8,6 +10,7 @@ import {
   AltStoreSourceShortcut,
   AltStoreConfig
 } from 'ducks/AlternativeStore/types'
+import { generateI18nConfig } from 'ducks/AlternativeStore/helpers'
 
 export const useAlternativeStore = (): {
   alternativeApps: AltStoreSourceShortcut[]
@@ -18,6 +21,9 @@ export const useAlternativeStore = (): {
     shortcutsQuery.options
   ) as { data: AltStoreSourceShortcut[]; fetchStatus: string }
   const config = flag('store.alternative-source') as AltStoreConfig | undefined
+
+  const i18nConfig = generateI18nConfig(config?.categories)
+  useExtendI18n(i18nConfig)
 
   const alternativeApps = useMemo(() => {
     if (fetchStatus !== 'loaded' || !config) {
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+declare module 'cozy-ui/transpiled/react/providers/I18n' {
+  export const useExtendI18n: (
+    translations: Record<string, Record<string, string>>
+  ) => void
+}
