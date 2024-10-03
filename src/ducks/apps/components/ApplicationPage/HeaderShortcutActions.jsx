13ba35d450787c4e52d254c81d9aa963b0007518
cballevre <celestin.ballevre@protonmail.com>
2024-10-03 18:03:25 +0200

    feat(ApplicationPage): Add / Remove additional apps from home page
    This new component is responsible for the lifecycle of additional apps that shortcuts under the hood

---
+++ src/ducks/apps/components/ApplicationPage/HeaderShortcutActions.jsx
@@ -0,0 +1,108 @@
+import cozySmileIcon from 'assets/icons/icon-cozy-smile.svg'
+import { buildFileFromPathQuery } from 'ducks/queries'
+import React, { useState } from 'react'
+
+import { useClient, useFetchShortcut } from 'cozy-client'
+import { move } from 'cozy-client/dist/models/file'
+import flag from 'cozy-flags'
+import Button from 'cozy-ui/transpiled/react/Buttons'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import { useAlert } from 'cozy-ui/transpiled/react/providers/Alert'
+import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+const HeaderShortcutActions = ({ app }) => {
+  const { isMobile } = useBreakpoints()
+  const { t } = useI18n()
+  const client = useClient()
+  const { shortcutInfos } = useFetchShortcut(client, app.id)
+  const { showAlert } = useAlert()
+
+  const [isBusy, setBusy] = useState(false)
+
+  const config = flag('store.alternative-source')
+
+  const handleAdd = async () => {
+    try {
+      setBusy(true)
+      // Install can be disabled
+      const shortcutCategory = app.categories[0]
+      const categoryPath = config.categories[shortcutCategory]
+      const fileQuery = buildFileFromPathQuery(categoryPath)
+      const categoryDirectory = await client.fetchQueryAndGetFromState(
+        fileQuery
+      )
+      await move(client, app, categoryDirectory.data[0])
+      showAlert({
+        message: t('HeaderShortcutActions.addSuccess'),
+        severity: 'success'
+      })
+    } catch {
+      showAlert({
+        message: t('HeaderShortcutActions.error'),
+        severity: 'error'
+      })
+    } finally {
+      setBusy(false)
+    }
+  }
+
+  const handleRemove = async () => {
+    try {
+      setBusy(true)
+      // Uninstall can be disabled
+      const storePath = config.store
+      const fileQuery = buildFileFromPathQuery(storePath)
+      const storeDirectory = await client.fetchQueryAndGetFromState(fileQuery)
+      await move(client, app, storeDirectory.data[0])
+      showAlert({
+        message: t('HeaderShortcutActions.removeSuccess'),
+        severity: 'success'
+      })
+    } catch {
+      showAlert({
+        message: t('HeaderShortcutActions.error'),
+        severity: 'error'
+      })
+    } finally {
+      setBusy(false)
+    }
+  }
+
+  const url = shortcutInfos?.data?.attributes?.url || ''
+
+  if (app.installed) {
+    return (
+      <>
+        <Button
+          href={url}
+          label={t('HeaderShortcutActions.open')}
+          className="c-btn"
+          disabled={isBusy}
+        />
+        <Button
+          variant="secondary"
+          fullWidth={isMobile}
+          className={isMobile ? 'u-mt-1' : null}
+          onClick={handleRemove}
+          // disabled={isUninstallDisabled}
+          label={t('HeaderShortcutActions.remove')}
+          busy={isBusy}
+        />
+      </>
+    )
+  }
+
+  return (
+    <Button
+      fullWidth={isMobile}
+      // disabled={isInstallDisabled}
+      onClick={handleAdd}
+      startIcon={<Icon icon={cozySmileIcon} />}
+      label={t('HeaderShortcutActions.add')}
+      busy={isBusy}
+    />
+  )
+}
+
+export { HeaderShortcutActions }
