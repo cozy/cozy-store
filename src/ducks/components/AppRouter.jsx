dea0b6157cf7eb30f9a9f7742fe24585c4cf28be
Théo Poizat <hello@zatteo.com>
2025-04-18 09:29:37 +0200

    fix: Fix search on mobile

--- src/ducks/components/AppRouter.jsx
+++ src/ducks/components/AppRouter.jsx
@@ -4,6 +4,7 @@ import IntentRedirect from 'ducks/components/intents/IntentRedirect'
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
+import { BarRoutes } from 'cozy-bar'
 const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes)
 
 export const AppRouter = () => {
@@ -12,6 +13,7 @@ export const AppRouter = () => {
       <Route path="redirect" element={<IntentRedirect />} />
       <Route path="discover/*" element={<Discover />} />
       <Route path="myapps/*" element={<MyApplications />} />
+      {BarRoutes.map(BarRoute => BarRoute)}
       <Route path="*" element={<Navigate replace to="discover" />} />
     </SentryRoutes>
   )
